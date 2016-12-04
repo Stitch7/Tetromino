13c832ba8a063b9cb27fd91d0c3fdb4825e0ea73
Christopher Reitz <christopher@reitz.re>
2016-12-04 17:07:25 +0100

    Basic Workspace setup

---
+++ TetrominoCoreMac/TetrominoCoreMac/TetrominoCoreMac.h
@@ -0,0 +1,19 @@
+//
+//  TetrominoCoreMac.h
+//  TetrominoCoreMac
+//
+//  Created by Christopher Reitz on 04/12/2016.
+//  Copyright © 2016 Christopher Reitz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TetrominoCoreMac.
+FOUNDATION_EXPORT double TetrominoCoreMacVersionNumber;
+
+//! Project version string for TetrominoCoreMac.
+FOUNDATION_EXPORT const unsigned char TetrominoCoreMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TetrominoCoreMac/PublicHeader.h>
+
+
